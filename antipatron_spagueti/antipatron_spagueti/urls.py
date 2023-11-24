c29c5f60e27f3dcea9fba9e794a6add28a1a2d40
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2023-11-24 16:48:02 +0000

    commit

--- antipatron_spagueti/antipatron_spagueti/urls.py
+++ antipatron_spagueti/antipatron_spagueti/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('calculadora/', include('calculadora.urls')),
+    path('', include('calculadora.urls')),
 ]
 
