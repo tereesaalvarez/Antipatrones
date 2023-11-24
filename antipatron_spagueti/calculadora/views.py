0a628de8fa86e6f30aaae32b7c811bd0b29dfabc
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2023-11-24 16:20:52 +0000

    commit

--- antipatron_spagueti/calculadora/views.py
+++ antipatron_spagueti/calculadora/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def calculadora(request):
+    return render(request, 'calculadora/calculadora.html')
